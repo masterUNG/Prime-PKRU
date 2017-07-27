6ebc02a9fd49bdf0d67d2277efe39f5dce690dad
MasterUNG <phrombutr@gmail.com>
2017-07-27 10:01:04 +0700

    Initial Commit

---
+++ Prime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prime
+//
+//  Created by MasterUNG on 7/27/2560 BE.
+//  Copyright © 2560 MasterUNG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
